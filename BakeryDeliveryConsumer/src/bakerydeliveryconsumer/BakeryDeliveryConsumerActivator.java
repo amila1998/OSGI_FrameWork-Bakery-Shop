1c4ed6b51d3d6a3ad2002d4c165be5dd9095a44d
amila1998 <amiladevin@gmail.com>
2022-03-24 23:07:43 +0530

    step 12

--- BakeryDeliveryConsumer/src/bakerydeliveryconsumer/BakeryDeliveryConsumerActivator.java
+++ BakeryDeliveryConsumer/src/bakerydeliveryconsumer/BakeryDeliveryConsumerActivator.java
@@ -1,22 +1,28 @@
 package bakerydeliveryconsumer;
 
+import bakerydeliveryproducer.BakeryDeliveryProducer;
+import bakeryitemsproducer.BakeryItemsProducer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+
+
 
 public class BakeryDeliveryConsumerActivator implements BundleActivator {
 
-	private static BundleContext context;
+	ServiceReference serviceReference,serviceReferencecustomerItems;
 
-	static BundleContext getContext() {
-		return context;
+	public void start(BundleContext context) throws Exception {
+		serviceReference = context.getServiceReference(BakeryDeliveryProducer.class.getName());
+		BakeryDeliveryProducer bakeryDeliveryProducer =  (BakeryDeliveryProducer) context.getService(serviceReference);
+		
+		serviceReferencecustomerItems = context.getServiceReference(BakeryItemsProducer.class.getName());
+		BakeryItemsProducer bakeryItemsProducer = (BakeryItemsProducer) context.getService(serviceReferencecustomerItems);
 	}
 
-	public void start(BundleContext bundleContext) throws Exception {
-		BakeryDeliveryConsumerActivator.context = bundleContext;
-	}
-
-	public void stop(BundleContext bundleContext) throws Exception {
-		BakeryDeliveryConsumerActivator.context = null;
+	public void stop(BundleContext context) throws Exception {
+		
 	}
 
 }
